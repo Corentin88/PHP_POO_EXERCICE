24195c87dcccd0184a3e5abc72b355f4618fd6f0
Corentin <Corentin.Lanaud@wanadoo.fr>
2025-02-21 08:51:30 +0100

    saut de ligne static

--- Vehicule.class.php
+++ Vehicule.class.php
@@ -3,15 +3,20 @@ abstract class Vehicule
 {
     protected $couleur;
     protected $poids;
+    
+    public const SAUT_DE_LIGNE = "<br>";
+
     public function __construct($couleur, $poids)
     {
         $this->couleur = $couleur;
         $this->poids = $poids;
     }
+
     public function rouler()
     {
-        echo '<h2 class="h2">Le véhicule roule.</h2><br>';
+        echo '<h2 class="h2">Le véhicule roule.</h2>' . self::SAUT_DE_LIGNE;
     }
+
     abstract public function ajouter_personne($poids_personne);
 
     public function getCouleur()
@@ -33,16 +38,20 @@ abstract class Vehicule
     {
         $this->poids = $poids;
     }
+
     public function repeindre($couleur)
     {
         $this->setCouleur($couleur);
-        return "Véhicule repeint en : " .$this->getCouleur();
+        echo "Véhicule repeint en : " . $this->getCouleur() . self::SAUT_DE_LIGNE;
     }
 
     public static function afficher_attribut(Vehicule $vehicule)
     {
-        echo "<pre>";
-        print_r($vehicule);
-        echo "</pre>";
+        echo "<strong>Attributs du véhicule :</strong>" . self::SAUT_DE_LIGNE;
+        echo "Couleur : " . $vehicule->getCouleur() . self::SAUT_DE_LIGNE;
+        echo "Poids : " . $vehicule->getPoids() . " kg" . self::SAUT_DE_LIGNE;
+
+        
     }
 }
+?>
